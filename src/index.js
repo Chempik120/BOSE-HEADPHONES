969f0f6c37192940d12db92b32e024c5ae41631b
Chempik120 <135869484+Chempik120@users.noreply.github.com>
2024-05-12 15:22:46 +0300

    п

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const img = document.querySelector('#buy-image');
+const whiteSVGFirst = document.querySelector('#circle-white-first');
+const whiteSVGSecond = document.querySelector('#circle-white-second');
+const blackSVGFirst = document.querySelector('#circle-black-first');
+const blackSVGSecond = document.querySelector('#circle-black-second');
 const imgWhite = document.querySelector('#buy-image-white');
 const whiteButton = document.querySelector('#buy-white-button');
 const blackButton = document.querySelector('#buy-black-button');
@@ -7,13 +11,23 @@ whiteButton.addEventListener('click', onClickWhite);
 blackButton.addEventListener('click', onClickBlack);
 
 function onClickBlack(e) {
-  // img.src = blackPhones;
+  whiteSVGFirst.classList.remove('visually-hidden');
+  whiteSVGSecond.classList.add('visually-hidden');
+
+  blackSVGFirst.classList.remove('visually-hidden');
+  blackSVGSecond.classList.add('visually-hidden');
+
   img.classList.remove('visually-hidden');
   imgWhite.classList.add('visually-hidden');
 }
 
 function onClickWhite(e) {
-  // img.src = whitePhones;
+  whiteSVGFirst.classList.add('visually-hidden');
+  whiteSVGSecond.classList.remove('visually-hidden');
+
+  blackSVGFirst.classList.add('visually-hidden');
+  blackSVGSecond.classList.remove('visually-hidden');
+
   img.classList.add('visually-hidden');
   imgWhite.classList.remove('visually-hidden');
 }
