ac3b2427ee903226b608351b378a073f9b6fa49f
Chempik120 <135869484+Chempik120@users.noreply.github.com>
2024-06-26 17:32:54 +0000

    Deploying to gh-pages from @ Chempik120/BOSE-HEADPHONES@66baa174fc32ca7bdb8ba15681444ca2bf2f2fd5 🚀

---
+++ index.2a877559.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuButton:document.querySelector(".header-burger"),closeMenuButton:document.querySelector(".close-button"),mobileMenu:document.querySelector(".mobile-menu")};function n(n){console.log(n),e.mobileMenu.classList.toggle("is-hidden")}e.openMenuButton.addEventListener("click",n),e.closeMenuButton.addEventListener("click",n)}();
+//# sourceMappingURL=index.2a877559.js.map
